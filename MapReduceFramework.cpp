8d2efdbb17edc497937801cb60726046026898b3
ahmadok <ahmad.okosh@mail.huji.ac.il>
2021-06-06 17:44:14 +0300

    created initial implementation of emit2,emit3

--- MapReduceFramework.cpp
+++ MapReduceFramework.cpp
@@ -5,8 +5,9 @@
 
 #define MUTEX_INIT_FAIL "system error: initializing mutex for thread failed\n"
 #define THREAD_INIT_FAIL "system error: creating thread failed\n"
- struct JobContext
+struct JobContext //todo : add to class
 {
+    pthread_mutex_t lock;
     OutputVec &outputVec;
     std::atomic<uint64_t> *counter;
     JobState state;
@@ -35,11 +36,12 @@ JobHandle startMapReduceJob(const MapReduceClient &client,
 {
     auto* counter = new std::atomic<uint64_t>(0);
     auto *threads = new pthread_t[multiThreadLevel];
-    auto *jobContext = new JobContext{outputVec, counter, {UNDEFINED_STAGE, 0}, threads, false};
+    pthread_mutex_t job_lock;
+    auto *jobContext = new JobContext{job_lock, outputVec, counter, {UNDEFINED_STAGE, 0}, threads, false};
     auto *contexts = new ThreadContext[multiThreadLevel];
     for (int i = 0; i < multiThreadLevel; ++i)
     {
-        pthread_mutex_t lock;
+        pthread_mutex_t lock; //todo : same lock for all?
         if (pthread_mutex_init(&lock, nullptr))
         {
             std::cerr << MUTEX_INIT_FAIL; //todo: exit then free
@@ -64,6 +66,31 @@ void getJobState(JobHandle job, JobState* state)
     auto* context = static_cast<JobContext*>(job);
     state->percentage = context->state.percentage;
     state->stage = context->state.stage;
+}
+
+void emit2(K2 *key, V2 *value, void *context)
+{
+    //todo : wrap in function
+    auto* tc = static_cast<ThreadContext*>(context);
+    pthread_mutex_lock(&tc->lock);
+    IntermediatePair pair;
+    pair.first = key;
+    pair.second = value;
+    tc->intermediateVec.push_back(pair);
+    pthread_mutex_unlock(&tc->lock);
+}
+
+void emit3(K3 *key, V3 *value, void *context)
+{
+    //todo : wrap in function
+    auto* jc = static_cast<JobContext*>(context);
+    pthread_mutex_lock(&jc->lock);
+    OutputPair pair;
+    pair.first = key;
+    pair.second = value;
+    jc->outputVec.push_back(pair);
+    pthread_mutex_unlock(&jc->lock);
+
 }
 
 
